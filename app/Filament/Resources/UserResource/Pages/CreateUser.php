ec5218919528abced547069cdfe88a25344678ab
Lyca Ubay <lycaubay28@gmail.com>
2025-05-23 09:04:22 +0800

    fixes

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -9,4 +9,18 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        unset($data['barangay_id']);
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $barangayId = $this->form->getState()['barangay_id'];
+        if ($barangayId) {
+            $this->record->barangays()->attach($barangayId);
+        }
+    }
 }
