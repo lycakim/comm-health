e69a28edb5eb4a93a50b13ee107c2c6f23e968b9
Lyca Ubay <lycaubay28@gmail.com>
2025-09-30 07:17:44 +0800

    Added ActivityLog Model

---
+++ app/Models/ActivityLog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ActivityLog extends Model
+{
+    use HasFactory;
+
+    protected $table = 'activity_log';
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'causer_id');
+    }
+}
