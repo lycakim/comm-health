207c4a560c6847cd0be772700ae3e6b4798ec89d
Lyca Ubay <lycaubay28@gmail.com>
2025-09-30 05:52:52 +0800

    added user activity log in program

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Spatie\Activitylog\LogOptions;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Program extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $guarded = [];
 
@@ -20,5 +23,18 @@ class Program extends Model
     public function barangay(): BelongsTo
     {
         return $this->belongsTo(Barangay::class);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['program_name', 'category_id', 'barangay_id'])
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs()
+            ->setDescriptionForEvent(function (string $eventName) {
+                $userName = Auth::user()?->name ?? 'System';
+                $programName = $this->name ?? 'unknown program';
+                return "{$userName} {$eventName} a program {$programName}";
+            });
     }
 }
