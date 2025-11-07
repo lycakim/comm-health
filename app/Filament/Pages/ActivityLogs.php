4f6d9ad5c3d452c3bc319972a01f570dc5d7aab2
Lyca Ubay <lycaubay28@gmail.com>
2025-11-07 09:09:04 +0800

    added MHO on activitylogs

--- app/Filament/Pages/ActivityLogs.php
+++ app/Filament/Pages/ActivityLogs.php
@@ -24,7 +24,7 @@ class ActivityLogs extends Page implements HasTable
 
     public static function canAccess(): bool
     {
-        return Auth::user()->isAdmin();
+        return Auth::user()->isAdmin() || Auth::user()->isMHO();
     }
 
     public function table(Table $table): Table
