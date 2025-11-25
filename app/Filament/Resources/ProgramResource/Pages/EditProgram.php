e13fd9ab1e2ab612d06c5dd7259428eaf6a502fc
Lyca Ubay <lycaubay28@gmail.com>
2025-11-25 15:53:02 +0800

    updates

--- app/Filament/Resources/ProgramResource/Pages/EditProgram.php
+++ app/Filament/Resources/ProgramResource/Pages/EditProgram.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\ProgramResource\Pages;
 
-use App\Filament\Resources\ProgramResource;
+use App\Models\User;
 use Filament\Actions;
+use App\Enums\RoleEnum;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\ProgramResource;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\AnnouncementNotification;
 
 class EditProgram extends EditRecord
 {
@@ -20,5 +24,42 @@ class EditProgram extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        $record = $this->getRecord();
+        
+        $recipients = User::whereIn('role', [
+            RoleEnum::BHW->value,
+            RoleEnum::MIDWIFE->value,
+        ])
+        ->whereHas('barangays', function ($query) use ($record) {
+            $query->where('barangays.id', $record->barangay_id);
+        })
+        ->get();
+
+        // Dispatch to queue if many recipients
+        if ($recipients->count() > 10) {
+            foreach ($recipients as $recipient) {
+                $recipient->notify(
+                    (new AnnouncementNotification([
+                        'type' => 'program',
+                        'title' => 'Program Updated',
+                        'message' => $record->name . ' has been updated.',
+                        'icon' => 'heroicon-o-cube',
+                        'status' => 'info',
+                    ]))->delay(now()->addSeconds(5))
+                );
+            }
+        } else {
+            Notification::send($recipients, new AnnouncementNotification([
+                'type' => 'program',
+                'title' => 'Program Updated',
+                'message' => $record->name . ' has been updated.',
+                'icon' => 'heroicon-o-cube',
+                'status' => 'info',
+            ]));
+        }
     }
 }
